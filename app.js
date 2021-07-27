868e7b1646f31f99300ec9cac92bf62b5fb615b0
Abhirup Sarkar <76419771+Abhirup-Sarkar@users.noreply.github.com>
2021-07-27 20:37:30 +0530

    bill amount JS

---
+++ app.js
@@ -0,0 +1,19 @@
+var bill = document.querySelector("#bill-amount__box");
+var nextButton = document.querySelector("#next-button");
+
+
+
+function billAmountHandler(){
+    if (bill.value > 0){
+        console.log("you have entered an valid bill amount");
+    }
+    else{
+        console.log("enter a valid amount");
+    }
+
+}
+nextButton.addEventListener("click", billAmountHandler)
+
+   
+        
+
