7649d550720bcf8790bcd2bb0a5abe796b2cfea4
Abhirup Sarkar <76419771+Abhirup-Sarkar@users.noreply.github.com>
2021-07-27 20:39:17 +0530

    beautify file extension

--- app.js
+++ app.js
@@ -3,17 +3,12 @@ var nextButton = document.querySelector("#next-button");
 
 
 
-function billAmountHandler(){
-    if (bill.value > 0){
+function billAmountHandler() {
+    if (bill.value > 0) {
         console.log("you have entered an valid bill amount");
-    }
-    else{
+    } else {
         console.log("enter a valid amount");
     }
 
 }
 nextButton.addEventListener("click", billAmountHandler)
-
-   
-        
-
