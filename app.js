30bd7161fa9a9a88811f0219bc51a64b098b1127
Abhirup Sarkar <76419771+Abhirup-Sarkar@users.noreply.github.com>
2021-12-19 04:29:02 +0530

    processing done

--- app.js
+++ app.js
@@ -2,29 +2,38 @@ var billAmount = document.querySelector("#bill-amount");
 var btnReturnChange = document.querySelector("#btn-return-change");
 var cashGiven = document.querySelector("#cash-given");
 var errorMessage = document.querySelector("#error-message");
+var noOfNotes = document.querySelectorAll(".no-of-notes");
 
-var denominationOfNotes = [2000,500,100,20,10,5,1];
+var denominationOfNotes = [2000, 500, 100, 20, 10, 5, 1];
 
 function billAmountHandler() {
     errorMessage.style.display = "none";
-    if (billAmount.value > 0) {       
-        if(cashGiven.value >= billAmount.value){
-            var returnChangeAmount = (cashGiven.value - billAmount.value);
-            console.log(returnChangeAmount);
-        }
-        else{
+    if (billAmount.value > 0) {
+
+        if (cashGiven >= billAmount) {
+            let returnChangeAmount = (cashGiven.value - billAmount.value);
+            calculateChange(returnChangeAmount);
+        } else {
             showMessage("You have paid less cash than the bill amount.\n DO YOU WANNA WASH PLATES???");
-        }   
-    } 
-    else {
+
+        }
+    } else {
         showMessage("Please enter a valid amount");
+
     }
-    
+
 }
-function calculateChange(){
-    
+
+function calculateChange(returnChangeAmount) {
+
+    for (i = 0; i < denominationOfNotes.length; i++) {
+        var numberOfNotes = Math.trunc(returnChangeAmount/denominationOfNotes[i]);
+        returnChangeAmount = (returnChangeAmount%denominationOfNotes[i]);
+        noOfNotes[i].innerText = numberOfNotes;
+    }
 }
-function showMessage(message){
+
+function showMessage(message) {
     errorMessage.style.display = "block";
     errorMessage.innerText = message;
 }
