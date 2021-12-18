28682bea32f6879f364bdcee774c5eeac00e242f
Abhirup Sarkar <76419771+Abhirup-Sarkar@users.noreply.github.com>
2021-12-18 17:22:50 +0530

    corrected variable names, added amount to be returned

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
-var billAmount = document.querySelector("#bill-amount__box");
-var nextButton = document.querySelector("#next-button");
+var billAmount = document.querySelector("#bill-amount");
+var btnReturnChange = document.querySelector("#btn-return-change");
+var cashGiven = document.querySelector("#cash-given")
 
 
 
 function billAmountHandler() {
-    if (bill.value > 0) {
-        console.log("you have entered an valid bill amount");
-    } else {
-        console.log("enter a valid amount");
+    if (billAmount.value > 0) {
+        if(cashGiven.value >= billAmount.value){
+            var returnChangeAmount = (cashGiven.value - billAmount.value)
+            console.log(returnChangeAmount)
+        }
+        else{
+            console.log("You have paid less cash than the bill amount")
+        }   
+    } 
+    else {
+        console.log("Please enter a valid amount");
     }
-
+    
 }
-nextButton.addEventListener("click", billAmountHandler)
+btnReturnChange.addEventListener("click", billAmountHandler)
